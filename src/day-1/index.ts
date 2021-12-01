0c971458a142e5c14fee903a1d5c7fbc4a10d489
Filippo Andrighetti <filippo.andrighetti@facile.it>
2021-12-01 21:43:31 +0100

    resolved day-1

--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -1,10 +1,35 @@
 import { flow } from "fp-ts/function";
 import * as TE from "fp-ts/TaskEither";
-import * as A from "fp-ts/Array";
 import { readFile } from "@app/utils/file";
 
-export const main = flow(
+const countMeasurements = (list: number[]): number =>
+  list
+    .filter((value, index, array) => value > array[index - 1])
+    .length;
+
+const mapMeasurements = (list: number[]): number[] =>
+  list
+    .map((value, index, array) => value + array[index - 1] + array[index - 2]);
+
+export const main1 = flow(
   readFile,
   TE.map((a: string) => a.split("\n")),
-  TE.traverseArray((a: string) => a)
+  TE.map((a: string[]) => a.map(a => parseInt(a, 10))),
+  TE.map(countMeasurements),
 );
+
+main1('src/day-1/input.txt')()
+  .then(a => console.log('First', a))
+  .catch(a => console.log('First', a));
+
+export const main2 = flow(
+  readFile,
+  TE.map((a: string) => a.split("\n")),
+  TE.map((a: string[]) => a.map(a => parseInt(a, 10))),
+  TE.map(mapMeasurements),
+  TE.map(countMeasurements),
+);
+
+main2('src/day-1/input.txt')()
+  .then(a => console.log('Second', a))
+  .catch(a => console.log('Second', a));
--- test/day-1/index.spec.ts
+++ test/day-1/index.spec.ts
@@ -1,17 +1,24 @@
 import * as E from "fp-ts/Either";
-import { main } from "@app/day-1";
+import { main1, main2 } from "@app/day-1";
 
 describe("AdventOfCode 2021 - Day 1", () => {
   it("Return error for non existing file", async () => {
-    const result = await main('src/day-1/non-exists.txt')();
+    const result = await main1('src/day-1/non-exists.txt')();
     const expected = E.left("ENOENT: no such file or directory, open 'src/day-1/non-exists.txt'");
 
     expect(result).toEqual(expected);
   });
 
   it("Return result 7 for first challenge", async () => {
-    const result = await main('test/day-1/input-1.txt')();
+    const result = await main1('test/day-1/input-1.txt')();
     const expected = E.right(7);
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Return result 5 for second challenge", async () => {
+    const result = await main2('test/day-1/input-1.txt')();
+    const expected = E.right(5);
 
     expect(result).toEqual(expected);
   });
